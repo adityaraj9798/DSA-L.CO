cbbf4b7cae3bb5df6d77ae7c993d646d62275c33
Aditya raj <adityaraj97989@gmail.com>
2026-09-01 14:40:51 +0530

    0122-BEST-TIME-TO-BUY-AND-SELL-STOCK-II Stats: Time: 0 ms (100%), Space: 19.9 MB (97.43%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int p=0;
+        int s=0;
         for(int i=1;i<prices.size();i++){
             if(prices[i]>prices[i-1]){
-                p+=prices[i]-prices[i-1];
+                s+=prices[i]-prices[i-1];
             }
         }
-        return p;
+        return s;
     }
 };
