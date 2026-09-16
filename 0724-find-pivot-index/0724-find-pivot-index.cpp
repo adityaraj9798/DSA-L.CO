b15b345ad1998aa6a0652ec73ac88ac457b3cdd9
Aditya raj <adityaraj97989@gmail.com>
2026-09-16 16:03:36 +0530

    0724-FIND-PIVOT-INDEX Stats: Time: 737 ms (6.11%), Space: 35.8 MB (54.51%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
-        int totalsum=0;
-        int leftsum=0;
-        for(int i=0;i<n;i++){
-            totalsum+=nums[i];
-        }
-        for(int i=0;i<n;i++){
-            int rightsum=totalsum-leftsum-nums[i];
-            if(leftsum==rightsum){
+        for (int i = 0; i < n; i++) {
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int j = 0; j < i; j++) {
+                leftSum += nums[j];
+            }
+            for (int j = i + 1; j < n; j++) {
+                rightSum += nums[j];
+            }
+            if (leftSum == rightSum) {
                 return i;
             }
-            leftsum+=nums[i]; 
         }
         return -1;
     }
